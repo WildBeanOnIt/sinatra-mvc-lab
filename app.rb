d75275225d7b0437d578a59d630c0a9b3d04bf26
Julio Aguilera Jr <julioaguilera337@gmail.com>
2022-03-16 21:02:31 -0500

    test is glitched out

--- app.rb
+++ app.rb
@@ -2,13 +2,16 @@ require_relative 'config/environment'
 require_relative 'models/piglatinizer.rb'
 class App < Sinatra::Base
 
-    get "/" do
+    get '/' do
         erb :user_input
     end
 
-    post '/' do
-        @pig_latinizer = PigLatinizer.new(params[:user_text])
+    post '/piglatinize' do
+        pl = Piglatinizer.new
+        @piglatin = pl.piglatinize(params[:user_phrase])
 
         erb :results
     end
+
+
 end
--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,8 +1,21 @@
 class PigLatinizer
 
-
-    def initialize
-        word = "pigLatinizer"
+    def piglatinize(input_str)
+        input_str.split(" ").map { |word| piglatinize_word(word)}.join(" ")
     end
 
+    def vowel?(char)
+        char.match(/[aAeEiIoOuU]/)
+    end
+
+    def piglatinize_word(word)
+        if vowel?(word[0])
+            word + "way"
+        else
+            vowel_index = word.index(/aAeEiIoOuU/)
+            consonants = word.slice(00.vowel_index-1)
+            word_leftover = word.slice(vowel_index..word.length)
+            word_leftover + consonants + "ay"
+        end
+    end
 end
