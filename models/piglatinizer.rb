e960f9e52f39bc68ab3ed18c41c7f36e77e6ae6a
Julio Aguilera Jr <julioaguilera337@gmail.com>
2022-03-17 20:07:31 -0500

    lab finally done

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,3 +1,4 @@
+
 class PigLatinizer
 
     def piglatinize(input_str)
@@ -9,12 +10,14 @@ class PigLatinizer
     def vowel?(char)
         char.match(/[aAeEiIoOuU]/)
     end
-    
+
     def piglatinize_word(word)
+        # word starts with vowel
         if vowel?(word[0])
             word + "way"
         else
-            vowel_index = word.index(/aAeEiIoOuU/)
+        # word starts with one or more consonants
+            vowel_index = word.index(/[aAeEiIoOuU]/)
             consonants = word.slice(0..vowel_index-1)
             word_leftover = word.slice(vowel_index..word.length)
             word_leftover + consonants + "ay"
